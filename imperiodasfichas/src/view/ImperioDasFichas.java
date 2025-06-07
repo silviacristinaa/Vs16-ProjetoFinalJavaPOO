73bd09d36c49b4747fdc3ab0930e0461f3681639
Lucas Feitosa Rossinhole <lucas.rossinhole@hotmail.com>
2025-06-07 14:08:32 -0300

    feat: atualizar classe ImperioDasFichas com menu de opções, sistema de login e cadastro e sistema do jogo Roleta.

--- imperiodasfichas/src/view/ImperioDasFichas.java
+++ imperiodasfichas/src/view/ImperioDasFichas.java
@@ -2,6 +2,8 @@ package view;
 
 import controller.GerenciadorJogo;
 import model.Jogador;
+import model.jogos.Jogo;
+import model.jogos.roletas.Roleta;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,37 +13,38 @@ public class ImperioDasFichas {
     static final GerenciadorJogo gerenciador = new GerenciadorJogo("Império das Fichas", 1.00, new ArrayList<>());
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+
         int opcao;
         boolean jogadorCadastrado = false;
+
+        // Adição do jogo "Roleta Clássica" no Array de jogos
+        Jogo roleta = new Roleta("Roleta Clássica", "Aposte em PAR (0) ou ÍMPAR (1). Se acertar, você ganha o dobro!");
+        gerenciador.adicionarJogo(roleta);
 
         do {
             System.out.println("\n=======================================");
             System.out.println("🎰  BEM-VINDO AO IMPÉRIO DAS FICHAS!  🎰");
             System.out.println("=======================================\n");
 
-            System.out.println("‍🎮  Menu jogador");
+            System.out.println("‍🎮  Menu Inicial\n");
 
             System.out.println("🆕  1. Cadastrar novo jogador");
-            // implementar realização de login
-            //System.out.println("🔐  2. Realizar login");
-
-            System.out.println("\n🚪  2. Desistir de tentar a sorte");
+            System.out.println("🔐  2. Realizar login");
+            System.out.println("🚪  3. Desistir de tentar a sorte");
 
             System.out.print("\n🧭 Escolha uma opção: ");
             opcao = Integer.parseInt(scanner.nextLine());
-
 
             switch (opcao) {
 
                 case 1:
                     System.out.println("Cadastrar jogador selecionado!");
 
-                    System.out.print("Nome:");
+                    System.out.print("Nome: ");
                     String nome = scanner.nextLine();
 
-                    System.out.print("Idade:");
+                    System.out.print("Idade: ");
                     int idade = Integer.parseInt(scanner.nextLine());
 
                     if (idade < 18) {
@@ -49,20 +52,23 @@ public class ImperioDasFichas {
                         break;
                     }
 
-                    System.out.print("Nickname:");
+                    System.out.print("Nickname: ");
                     String nickname = scanner.nextLine();
 
-<<<<<<< HEAD
                     if (!gerenciador.adicionarJogador(nome, idade, nickname)) {
-=======
-                    if (!gerenciador.adicionarJogador(new Jogador(nome, idade, nickname))) {
->>>>>>> 4cd5fd3c38df71aa7dd4650cf110c36ada076b84
                         System.out.println("Erro. Já existe um jogador com esse nickname.");
                         break;
                     }
 
-                    menuCarteira();
+                    Jogador jogadorNovo = gerenciador.buscarJogador(nickname);
+                    menuOpcoesJogador(jogadorNovo);
 
+                    break;
+                case 2:
+                    controleLogin();
+                    break;
+                case 3:
+                    System.out.println("\n👋 Obrigado por jogar! Volte sempre!");
                     break;
 
                 default:
@@ -70,90 +76,198 @@ public class ImperioDasFichas {
                     break;
             }
 
-        } while (opcao != 2);
+        } while (opcao != 3);
 
         scanner.close();
     }
 
-    public static void menuCarteira() {
-
+    public static void menuCarteira(Jogador jogador) {
         Scanner scanner = new Scanner(System.in);
         int opcao;
-        String nickname;
-        Jogador jogador;
 
         do {
-            System.out.println("💼  Carteira Virtual");
-            System.out.println("💰  1. Depositar dinheiro");
-            System.out.println("💸  2. Sacar dinheiro");
-            System.out.println("🎟️  3. Depositar fichas");
-            System.out.println("🎯  4. Sacar fichas");
+            System.out.printf("\n💼 Carteira Virtual do Jogador %s\n", jogador.getNickname());
 
-            System.out.println("\n🚪  5. Retornar");
+            System.out.println(jogador.getCarteira() + "\n");
+
+            System.out.println("💰 1. Depositar dinheiro");
+            System.out.println("💸 2. Sacar dinheiro");
+            System.out.println("🎟️ 3. Comprar fichas");
+            System.out.println("🎯 4. Vender fichas");
+            System.out.println("🚪 5. Retornar");
 
             System.out.print("\n🧭 Escolha uma opção: ");
-
             opcao = Integer.parseInt(scanner.nextLine());
 
-            System.out.println("Digite o nickname: ");
-            nickname = scanner.nextLine();
-
-            jogador = gerenciador.buscarJogador(nickname);
-
-            if (jogador == null) {
-                System.out.println("Jogador não encontrado.");
-                break;
-            }
-
             switch (opcao) {
-
                 case 1:
                     System.out.print("\n💰 Digite o valor do depósito: R$ ");
                     double deposito = Double.parseDouble(scanner.nextLine());
 
                     if (!jogador.getCarteira().depositarDinheiro(deposito)) {
-                        System.out.println("Valor inválido. Somente valores positivos.");
-                        break;
+                        System.out.println("❌ Valor inválido. Somente valores positivos.");
+                    } else {
+                        System.out.println("✅ Valor de R$ " + deposito + " depositado com sucesso!");
                     }
-
-                    System.out.println("✅ Valor de R$ " + deposito + " depositado com sucesso!");
                     break;
 
                 case 2:
                     System.out.print("\n💸 Digite o valor que deseja sacar: R$ ");
                     double saque = Double.parseDouble(scanner.nextLine());
 
-                    if (!jogador.getCarteira().sacarDinheiro(saque)) {
-                        System.out.println("Valor inválido. Saldo insuficiente.");
+                    if (saque <= 0) {
+                        System.out.println("❌ Valor inválido! Somente valores positivos.");
                         break;
                     }
 
-                    System.out.println("✅ Valor de R$ " + saque + " sacado com sucesso!");
+                    if (!jogador.getCarteira().sacarDinheiro(saque)) {
+                        System.out.println("❌ Valor inválido. Saldo insuficiente.");
+                    } else {
+                        System.out.println("✅ Valor de R$ " + saque + " sacado com sucesso!");
+                    }
                     break;
 
                 case 3:
-                    System.out.println("\n🎟️  Comprar fichas selecionado!");
+                    System.out.print("\n🎟️  Digite o número de fichas que deseja comprar: ");
+                    int qtdComprar = Integer.parseInt(scanner.nextLine());
 
-                    // lógica do depósito de fichas
+                    if (qtdComprar <= 0) {
+                        System.out.println("❌ Valor inválido! Somente valores positivos.");
+                        break;
+                    }
 
+                    if (!gerenciador.comprarFicha(jogador.getNickname(), qtdComprar)) {
+                        System.out.println("❌ Dinheiro insuficiente para comprar as fichas!");
+                    } else {
+                        System.out.println("✅ Compra de " + qtdComprar + " fichas realizada com sucesso!");
+                    }
                     break;
 
                 case 4:
-                    System.out.println("\n🎯 Vender fichas selecionado!");
+                    System.out.print("\n🎯 Digite o número de fichas que deseja vender: ");
+                    int qtdVender = Integer.parseInt(scanner.nextLine());
 
-                    // lógica do saque de fichas
+                    if (qtdVender <= 0) {
+                        System.out.println("❌ Valor inválido! Somente valores positivos.");
+                        break;
+                    }
 
+                    if (!gerenciador.venderFicha(jogador.getNickname(), qtdVender)) {
+                        System.out.println("❌ Não foi possível vender as fichas.");
+                    } else {
+                        System.out.println("✅ Venda de " + qtdVender + " fichas realizada com sucesso!");
+                    }
                     break;
 
                 case 5:
-                    System.out.println("\n👋 Obrigado por jogar no Império das Fichas e tentar a sua sorte. Até logo!");
+                    System.out.println("\n👋 Voltando ao Menu de Opções do Jogador...");
                     break;
 
                 default:
                     System.out.println("\n⚠️  Opção inválida! Por favor, tente novamente.");
                     break;
             }
-
         } while (opcao != 5);
     }
+
+    public static void menuOpcoesJogador(Jogador jogador) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("\n O QUE DESEJA FAZER?\n");
+            System.out.println("💼 1. Acessar Carteira");
+            System.out.println("🎰 2. Jogar Roleta");
+            System.out.println("🚪 3. Voltar ao Menu Inicial...");
+            System.out.print("\n🧭 Escolha uma opção: ");
+            opcao = Integer.parseInt(scanner.nextLine());
+
+            switch (opcao) {
+                case 1:
+                    menuCarteira(jogador);
+                    break;
+                case 2:
+                    menuRoleta(jogador);
+                    break;
+                case 3:
+                    System.out.println("\n👋 Retornando ao Menu Principal...");
+                    break;
+                default:
+                    System.out.println("\n⚠️  Opção inválida! Por favor, tente novamente.");
+                    break;
+            }
+        } while (opcao != 3);
+    }
+
+    public static void controleLogin() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n🔐 Império das Fichas - Sistema de Login");
+
+        System.out.print("Digite seu nickname: ");
+        String nickname = scanner.nextLine();
+
+        Jogador jogador = gerenciador.buscarJogador(nickname);
+
+        if (jogador == null) {
+            System.out.println("❌ Jogador não encontrado. Verifique o nickname ou cadastre-se primeiro.");
+            return;
+        }
+
+        System.out.printf("✅ Login realizado com sucesso! Bem-vindo, %s!!\n", jogador.getNickname());
+        menuOpcoesJogador(jogador);
+    }
+
+    public static void menuRoleta(Jogador jogador) {
+        Scanner scanner = new Scanner(System.in);
+
+        Jogo roleta = gerenciador.buscarJogo("Roleta Clássica");
+
+        if (roleta == null) {
+            System.out.println("❌ Roleta não está disponível no momento.");
+            return;
+        }
+
+        System.out.println("\n🎰 Bem-vindo à Roleta Clássica!");
+        System.out.println("\n==========================================================================================");
+        roleta.exibirRegras();
+        System.out.println("==========================================================================================\n");
+        System.out.printf("🎟️ Você tem %d fichas.\n", jogador.getCarteira().getFichas());
+        System.out.print("Quantas fichas deseja apostar?\n");
+        System.out.print("\nFICHAS APOSTADAS: ");
+        int valorApostado = Integer.parseInt(scanner.nextLine());
+
+        if (valorApostado <= 0 || valorApostado < roleta.getValorInicial()) {
+            System.out.println("\n=======================================================");
+            System.out.printf("❌ Aposta inválida. Mínimo: %d fichas.\n", roleta.getValorInicial());
+            System.out.println("=======================================================");
+            return;
+        }
+        if (valorApostado > jogador.getCarteira().getFichas()) {
+            System.out.println("❌ Você não tem fichas suficientes para essa aposta.");
+            return;
+        }
+
+        System.out.println("\nEscolha sua aposta:");
+        System.out.println("Digite '0' para escolher PAR");
+        System.out.println("Digite '1' para escolher ÍMPAR");
+        System.out.print("\nSUA ESCOLHA: ");
+        int escolha = Integer.parseInt(scanner.nextLine());
+
+        if (!roleta.apostaValida(valorApostado, escolha)) {
+            System.out.println("❌ Aposta cancelada.");
+            return;
+        }
+
+        jogador.getCarteira().sacarFichas(valorApostado);
+
+        boolean ganhou = roleta.jogar(valorApostado, escolha);
+
+        if (ganhou) {
+            int premio = valorApostado * 2;
+            jogador.getCarteira().depositarFichas(premio);
+        }
+    }
+
 }
+

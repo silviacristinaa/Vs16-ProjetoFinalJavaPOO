2a4141a69095296e18837f62c0afec7d47ac7c29
will-developer <william_limaa@hotmail.com>
2025-06-11 12:35:13 -0300

    feat(util) criar classe das cores para utilizar nas classes de interface e melhorar a estilizacao

---
+++ imperiodasfichas/src/view/utils/AnsiColors.java
@@ -0,0 +1,13 @@
+// view/utils/AnsiColors.java
+package view.utils;
+
+public class AnsiColors {
+    public static final String RESET = "\u001B[0m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String RED = "\u001B[31m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String WHITE_BG = "\u001B[47m";
+    public static final String BLACK = "\u001B[30m";
+}

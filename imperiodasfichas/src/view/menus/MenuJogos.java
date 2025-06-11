0df3c07f922e46bb8804016233067a1e77c8e204
will-developer <william_limaa@hotmail.com>
2025-06-11 12:39:50 -0300

    feat: incluir o metodo limpar terminal e estilizar as cores na classe MenuJogos

--- imperiodasfichas/src/view/menus/MenuJogos.java
+++ imperiodasfichas/src/view/menus/MenuJogos.java
@@ -4,6 +4,8 @@ import controller.GerenciadorJogo;
 import model.Jogador;
 import model.jogos.Jogo;
 import view.ImperioDasFichas;
+import view.utils.LimparTerminal;
+import view.utils.AnsiColors;
 
 import java.util.Scanner;
 
@@ -12,10 +14,10 @@ public class MenuJogos {
     public static void executarMenu(Jogador jogador, GerenciadorJogo gerenciadorJogo) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n🎰 Escolha o tipo de Roleta:");
-        System.out.println("1. Roleta Clássica (⚪ Par/⚫ Ímpar)");
-        System.out.println("2. Roleta das Cores 🌈");
-        System.out.print("\n🧭 Escolha uma opção: ");
+        System.out.println("\n" + AnsiColors.PURPLE + "🎰 Escolha o tipo de Roleta:" + AnsiColors.RESET);
+        System.out.println(AnsiColors.GREEN + "1. Roleta Clássica (⚪ Par/⚫ Ímpar)" + AnsiColors.RESET);
+        System.out.println(AnsiColors.GREEN + "2. Roleta das Cores 🌈" + AnsiColors.RESET);
+        System.out.print("\n" + AnsiColors.YELLOW + "🧭 Escolha uma opção: " + AnsiColors.RESET);
         int opcaoEscolhida = ImperioDasFichas.lerInteiro(scanner.nextLine());
         Jogo jogoSelecionado = null;
 
@@ -24,7 +26,7 @@ public class MenuJogos {
                 try {
                     jogoSelecionado = gerenciadorJogo.buscarJogo("Roleta Clássica");
                 } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                    System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
                     return;
                 }
                 break;
@@ -32,153 +34,153 @@ public class MenuJogos {
                 try {
                     jogoSelecionado = gerenciadorJogo.buscarJogo("Roleta das Cores");
                 } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                    System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
                     return;
                 }
                 break;
             default:
-                System.out.println("❌ Opção inválida.");
+                System.out.println(AnsiColors.RED + "❌ Opção inválida." + AnsiColors.RESET);
                 break;
         }
 
         if (jogoSelecionado == null) {
-            System.out.println("❌ Roleta não está disponível no momento.");
+            System.out.println(AnsiColors.RED + "❌ Roleta não está disponível no momento." + AnsiColors.RESET);
             return;
         }
 
-        System.out.println("\n🎰 Bem-vindo à " + jogoSelecionado.getNomeJogo() + "!");
-        System.out.println("\n==========================================================================================");
+        System.out.println("\n" + AnsiColors.PURPLE + "🎰 Bem-vindo à " + jogoSelecionado.getNomeJogo() + "!" + AnsiColors.RESET);
+        System.out.println("\n" + AnsiColors.YELLOW + "==========================================================================================" + AnsiColors.RESET);
         jogoSelecionado.exibirRegras();
-        System.out.println("==========================================================================================\n");
-        System.out.printf("🎟️ Você tem %d fichas.\n", jogador.getCarteira().getFichas());
-        System.out.print("Quantas fichas deseja apostar?\n");
-        System.out.print("\nFICHAS APOSTADAS: ");
+        System.out.println(AnsiColors.YELLOW + "==========================================================================================" + AnsiColors.RESET + "\n");
+        System.out.printf(AnsiColors.CYAN + "🎟️ Você tem %d fichas.\n" + AnsiColors.RESET, jogador.getCarteira().getFichas());
+        System.out.print(AnsiColors.YELLOW + "Quantas fichas deseja apostar?\n" + AnsiColors.RESET);
+        System.out.print("\n" + AnsiColors.CYAN + "FICHAS APOSTADAS: " + AnsiColors.RESET);
         int valorApostado = ImperioDasFichas.lerInteiro(scanner.nextLine());
 
         if (valorApostado <= 0 || valorApostado < jogoSelecionado.getValorInicial()) {
-            System.out.println("\n=======================================================");
-            System.out.printf("❌ Aposta inválida. Mínimo: %d fichas.\n", jogoSelecionado.getValorInicial());
-            System.out.println("=======================================================");
+            System.out.println("\n" + AnsiColors.RED + "=======================================================" + AnsiColors.RESET);
+            System.out.printf(AnsiColors.RED + "❌ Aposta inválida. Mínimo: %d fichas.\n" + AnsiColors.RESET, jogoSelecionado.getValorInicial());
+            System.out.println(AnsiColors.RED + "=======================================================" + AnsiColors.RESET);
             return;
         }
         if (valorApostado > jogador.getCarteira().getFichas()) {
-            System.out.println("❌ Você não tem fichas suficientes para essa aposta.");
+            System.out.println(AnsiColors.RED + "❌ Você não tem fichas suficientes para essa aposta." + AnsiColors.RESET);
             return;
         }
 
         int escolha = -1;
-        // Da pra exibir as opções de aposta de acordo com o tipo de roleta.
-        // Um metodo na classe Jogo poderia printar as opçoes.
         if (opcaoEscolhida == 1) {
-            System.out.println("\nEscolha sua aposta:");
-            System.out.println("Digite '0' para escolher PAR");
-            System.out.println("Digite '1' para escolher ÍMPAR");
-            System.out.print("\nSUA ESCOLHA: ");
+            System.out.println("\n" + AnsiColors.YELLOW + "Escolha sua aposta:" + AnsiColors.RESET);
+            System.out.println(AnsiColors.GREEN + "Digite '0' para escolher PAR" + AnsiColors.RESET);
+            System.out.println(AnsiColors.GREEN + "Digite '1' para escolher ÍMPAR" + AnsiColors.RESET);
+            System.out.print("\n" + AnsiColors.CYAN + "SUA ESCOLHA: " + AnsiColors.RESET);
             escolha = ImperioDasFichas.lerInteiro(scanner.nextLine());
         } else if (opcaoEscolhida == 2) {
-            System.out.println("\nEscolha sua cor:");
-            System.out.println("Digite '0' para VERMELHO");
-            System.out.println("Digite '1' para AZUL");
-            System.out.println("Digite '2' para AMARELO");
-            System.out.println("Digite '3' para VERDE");
-            System.out.print("\nSUA ESCOLHA: ");
+            System.out.println("\n" + AnsiColors.YELLOW + "Escolha sua cor:" + AnsiColors.RESET);
+            System.out.println(AnsiColors.GREEN + "Digite '0' para VERMELHO" + AnsiColors.RESET);
+            System.out.println(AnsiColors.GREEN + "Digite '1' para AZUL" + AnsiColors.RESET);
+            System.out.println(AnsiColors.GREEN + "Digite '2' para AMARELO" + AnsiColors.RESET);
+            System.out.println(AnsiColors.GREEN + "Digite '3' para VERDE" + AnsiColors.RESET);
+            System.out.print("\n" + AnsiColors.CYAN + "SUA ESCOLHA: " + AnsiColors.RESET);
             escolha = ImperioDasFichas.lerInteiro(scanner.nextLine());
         }
 
         try {
             jogoSelecionado.apostaValida(valorApostado, escolha);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("❌ Aposta cancelada.");
+            System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
+            System.out.println(AnsiColors.RED + "❌ Aposta cancelada." + AnsiColors.RESET);
             return;
         }
 
         try {
             gerenciadorJogo.iniciarPartida(jogoSelecionado, jogador, valorApostado, escolha);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
         }
     }
 
     public static void menuCacaNiquel(Jogador jogador, GerenciadorJogo gerenciadorJogo) {
+        LimparTerminal.executar();
         Scanner scanner = new Scanner(System.in);
 
         Jogo cacaNiquel = null;
         try {
             cacaNiquel = gerenciadorJogo.buscarJogo("Caça Níquel");
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
         }
 
-        System.out.println("\n🎰 Bem-vindo ao " + cacaNiquel.getNomeJogo() + "!");
-        System.out.println("\n==========================================================================================");
+        System.out.println("\n" + AnsiColors.PURPLE + "🎰 Bem-vindo ao " + cacaNiquel.getNomeJogo() + "!" + AnsiColors.RESET);
+        System.out.println("\n" + AnsiColors.YELLOW + "==========================================================================================" + AnsiColors.RESET);
         cacaNiquel.exibirRegras();
-        System.out.println("==========================================================================================\n");
-        System.out.printf("🎟️ Você tem %d fichas.\n", jogador.getCarteira().getFichas());
-        System.out.print("Quantas fichas deseja apostar?\n");
-        System.out.print("\nFICHAS APOSTADAS: ");
+        System.out.println(AnsiColors.YELLOW + "==========================================================================================" + AnsiColors.RESET + "\n");
+        System.out.printf(AnsiColors.CYAN + "🎟️ Você tem %d fichas.\n" + AnsiColors.RESET, jogador.getCarteira().getFichas());
+        System.out.print(AnsiColors.YELLOW + "Quantas fichas deseja apostar?\n" + AnsiColors.RESET);
+        System.out.print("\n" + AnsiColors.CYAN + "FICHAS APOSTADAS: " + AnsiColors.RESET);
         int valorApostado = ImperioDasFichas.lerInteiro(scanner.nextLine());
 
         if (valorApostado <= 0 || valorApostado < cacaNiquel.getValorInicial()) {
-            System.out.println("\n=======================================================");
-            System.out.printf("❌ Aposta inválida. Mínimo: %d fichas.\n", cacaNiquel.getValorInicial());
-            System.out.println("=======================================================");
+            System.out.println("\n" + AnsiColors.RED + "=======================================================" + AnsiColors.RESET);
+            System.out.printf(AnsiColors.RED + "❌ Aposta inválida. Mínimo: %d fichas.\n" + AnsiColors.RESET, cacaNiquel.getValorInicial());
+            System.out.println(AnsiColors.RED + "=======================================================" + AnsiColors.RESET);
             return;
         }
         if (valorApostado > jogador.getCarteira().getFichas()) {
-            System.out.println("❌ Você não tem fichas suficientes para essa aposta.");
+            System.out.println(AnsiColors.RED + "❌ Você não tem fichas suficientes para essa aposta." + AnsiColors.RESET);
             return;
         }
 
-        System.out.println("\nPressione enter para apertar a alavanca:");
+        System.out.println("\n" + AnsiColors.YELLOW + "Pressione enter para apertar a alavanca:" + AnsiColors.RESET);
         scanner.nextLine();
 
         try {
             cacaNiquel.apostaValida(valorApostado, 0);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("❌ Aposta cancelada.");
+            System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
+            System.out.println(AnsiColors.RED + "❌ Aposta cancelada." + AnsiColors.RESET);
             return;
         }
 
         try {
             gerenciadorJogo.iniciarPartida(cacaNiquel, jogador, valorApostado, 0);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
         }
     }
 
     public static void menuBlackJack(Jogador jogador, GerenciadorJogo gerenciadorJogo) {
+        LimparTerminal.executar();
         Scanner scanner = new Scanner(System.in);
 
         Jogo blackJack = null;
         try {
             blackJack = gerenciadorJogo.buscarJogo("BlackJack");
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
             return;
         }
 
-        System.out.println("\n🃏 Bem-vindo ao " + blackJack.getNomeJogo() + "!");
+        System.out.println("\n" + AnsiColors.PURPLE + "🃏 Bem-vindo ao " + blackJack.getNomeJogo() + "!" + AnsiColors.RESET);
         blackJack.exibirRegras();
-        System.out.printf("🎟️ Você tem %d fichas.\n", jogador.getCarteira().getFichas());
-        System.out.print("Quantas fichas deseja apostar?\n");
-        System.out.print("\nFICHAS APOSTADAS: ");
+        System.out.printf(AnsiColors.CYAN + "🎟️ Você tem %d fichas.\n" + AnsiColors.RESET, jogador.getCarteira().getFichas());
+        System.out.print(AnsiColors.YELLOW + "Quantas fichas deseja apostar?\n" + AnsiColors.RESET);
+        System.out.print("\n" + AnsiColors.CYAN + "FICHAS APOSTADAS: " + AnsiColors.RESET);
         int valorApostado = ImperioDasFichas.lerInteiro(scanner.nextLine());
 
         if (valorApostado <= 0 || valorApostado < blackJack.getValorInicial()) {
-            System.out.printf("❌ Aposta inválida. Mínimo: %d fichas.\n", blackJack.getValorInicial());
+            System.out.printf(AnsiColors.RED + "❌ Aposta inválida. Mínimo: %d fichas.\n" + AnsiColors.RESET, blackJack.getValorInicial());
             return;
         }
         if (valorApostado > jogador.getCarteira().getFichas()) {
-            System.out.println("❌ Você não tem fichas suficientes para essa aposta.");
+            System.out.println(AnsiColors.RED + "❌ Você não tem fichas suficientes para essa aposta." + AnsiColors.RESET);
             return;
         }
 
         try {
             gerenciadorJogo.iniciarPartida(blackJack, jogador, valorApostado, 0);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(AnsiColors.RED + e.getMessage() + AnsiColors.RESET);
         }
     }
 }

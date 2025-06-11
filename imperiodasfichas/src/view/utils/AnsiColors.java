1079894cc0f61d482a30baae537ae3f56e38ed3f
will-developer <william_limaa@hotmail.com>
2025-06-11 12:37:17 -0300

    fix: remover cores inutilizadas

--- imperiodasfichas/src/view/utils/AnsiColors.java
+++ imperiodasfichas/src/view/utils/AnsiColors.java
@@ -1,4 +1,3 @@
-// view/utils/AnsiColors.java
 package view.utils;
 
 public class AnsiColors {
@@ -8,6 +7,4 @@ public class AnsiColors {
     public static final String GREEN = "\u001B[32m";
     public static final String RED = "\u001B[31m";
     public static final String PURPLE = "\u001B[35m";
-    public static final String WHITE_BG = "\u001B[47m";
-    public static final String BLACK = "\u001B[30m";
 }

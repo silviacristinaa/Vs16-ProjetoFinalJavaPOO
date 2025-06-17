f9d18b4ec8f2c22fe182a2f549a6d4a434085285
will-developer <william_limaa@hotmail.com>
2025-06-17 11:39:04 -0300

    feat: adicionar inserts da tabela carteira em dados.sql

--- bd/dados.sql
+++ bd/dados.sql
@@ -31,3 +31,26 @@ INSERT INTO Jogador (ID, nome, nickname, idade)
 VALUES(SEQ_JOGADOR.nextval, 'Cristina', 'cris', 40);
 INSERT INTO Jogador (ID, nome, nickname, idade)
 VALUES(SEQ_JOGADOR.nextval, 'Oscar', 'tito', 18);
+
+-- Inserção de dados na tabela "Carteira"
+
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 20, 100.00, 1);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 15, 50.00, 2);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 10, 80.00, 3);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 25, 20.00, 4);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 30, 150.00, 5);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 12, 60.00, 6);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 18, 40.00, 7);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 22, 90.00, 8);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 16, 75.00, 9);
+INSERT INTO Carteira (ID, fichas, dinheiro, id_jogador)
+VALUES (SEQ_CARTEIRA.NEXTVAL, 14, 55.00, 10);

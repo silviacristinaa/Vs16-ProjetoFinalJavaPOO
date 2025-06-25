9f3ca2f3be2e49fac69042ba833cdf2444ba1db8
will-developer <william_limaa@hotmail.com>
2025-06-25 14:11:26 -0300

    feat: inicializar service de carteira com o metodo listar e o metodo adicionar

--- src/main/java/br/com/dbc/vemser/imperiodasfichas/services/CarteiraService.java
+++ src/main/java/br/com/dbc/vemser/imperiodasfichas/services/CarteiraService.java
@@ -1,4 +1,26 @@
 package br.com.dbc.vemser.imperiodasfichas.services;
 
+import br.com.dbc.vemser.imperiodasfichas.entities.CarteiraEntity;
+import br.com.dbc.vemser.imperiodasfichas.exceptions.RegraDeNegocioException;
+import br.com.dbc.vemser.imperiodasfichas.repositories.CarteiraRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarteiraService {
+
+    private final CarteiraRepository carteiraRepository;
+
+    public CarteiraService(CarteiraRepository carteiraRepository) {
+        this.carteiraRepository = carteiraRepository;
+    }
+
+    public CarteiraEntity adicionarCarteira(CarteiraEntity carteira) throws RegraDeNegocioException {
+        return carteiraRepository.adicionar(carteira);
+    }
+
+    public List<CarteiraEntity> listar() throws RegraDeNegocioException {
+        return carteiraRepository.listar();
+    }
 }

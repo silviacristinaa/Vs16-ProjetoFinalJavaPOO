737cc493375b6cbdf35cc70687394d9730aec15f
will-developer <william_limaa@hotmail.com>
2025-06-25 17:04:10 -0300

    feat: adicionar carteira automaticamente ao criar Jogador

--- src/main/java/br/com/dbc/vemser/imperiodasfichas/controllers/CarteiraController.java
+++ src/main/java/br/com/dbc/vemser/imperiodasfichas/controllers/CarteiraController.java
@@ -3,6 +3,8 @@ package br.com.dbc.vemser.imperiodasfichas.controllers;
 import br.com.dbc.vemser.imperiodasfichas.entities.CarteiraEntity;
 import br.com.dbc.vemser.imperiodasfichas.exceptions.RegraDeNegocioException;
 import br.com.dbc.vemser.imperiodasfichas.services.CarteiraService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

8cedd24b3f694ac73fed39a9128de470ecbc4de9
will-developer <william_limaa@hotmail.com>
2025-06-29 13:13:11 -0300

    feat: implementar Schema do Swagger em PartidaDTOs

--- src/main/java/br/com/dbc/vemser/imperiodasfichas/dtos/PartidaResponseDTO.java
+++ src/main/java/br/com/dbc/vemser/imperiodasfichas/dtos/PartidaResponseDTO.java
@@ -1,8 +1,10 @@
 package br.com.dbc.vemser.imperiodasfichas.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class PartidaResponseDTO extends PartidaRequestDTO {
+    @Schema(description = "ID único da partida", example = "1")
     private Integer idPartida;
 }

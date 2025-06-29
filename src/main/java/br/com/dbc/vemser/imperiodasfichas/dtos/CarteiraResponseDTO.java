8bc09944dbc606d7510328a6172847e94389d553
will-developer <william_limaa@hotmail.com>
2025-06-29 13:13:45 -0300

    feat: implementar Schema do Swagger em CariraDTOs e JogadaDTO

--- src/main/java/br/com/dbc/vemser/imperiodasfichas/dtos/CarteiraResponseDTO.java
+++ src/main/java/br/com/dbc/vemser/imperiodasfichas/dtos/CarteiraResponseDTO.java
@@ -1,5 +1,6 @@
 package br.com.dbc.vemser.imperiodasfichas.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -7,8 +8,10 @@ import javax.validation.constraints.NotNull;
 @Data
 public class CarteiraResponseDTO extends CarteiraRequestDTO {
     @NotNull
+    @Schema(description = "ID único da carteira", example = "1")
     private Integer idCarteira;
 
     @NotNull
+    @Schema(description = "ID do jogador associado à carteira", example = "1")
     private Integer idJogador;
 }

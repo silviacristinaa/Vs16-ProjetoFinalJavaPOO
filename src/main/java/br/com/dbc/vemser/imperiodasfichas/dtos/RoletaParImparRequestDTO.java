1c42a0f92da1b804278c1e9dc45823fb2d82d793
will-developer <william_limaa@hotmail.com>
2025-06-29 13:13:00 -0300

    feat: implementar Schema do Swagger em RoletasDTOs

--- src/main/java/br/com/dbc/vemser/imperiodasfichas/dtos/RoletaParImparRequestDTO.java
+++ src/main/java/br/com/dbc/vemser/imperiodasfichas/dtos/RoletaParImparRequestDTO.java
@@ -1,5 +1,6 @@
 package br.com.dbc.vemser.imperiodasfichas.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.Max;
@@ -10,14 +11,17 @@ import javax.validation.constraints.NotNull;
 public class RoletaParImparRequestDTO {
 
     @NotNull
+    @Schema(description = "ID do Jogador", required = true, example = "1")
     private Integer idJogador;
 
     @NotNull
     @Min(1)
+    @Schema(description = "Quantidade de fichas a serem apostadas", required = true, example = "10")
     private Integer quantidadeFichasApostado;
 
     @NotNull
     @Min(value = 0, message = "Valor deve ser 0 para par ou 1 para impar")
     @Max(value = 1, message = "Valor deve ser 0 para par ou 1 para impar")
+    @Schema(description = "Opção de aposta (0 para PAR, 1 para ÍMPAR)", required = true, example = "0")
     private Integer opcao;
 }

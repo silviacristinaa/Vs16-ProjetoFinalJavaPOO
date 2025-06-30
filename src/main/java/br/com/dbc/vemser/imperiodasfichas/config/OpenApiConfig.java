b6a1233ff0010d6e610b5ae4bb20fba280db4e15
will-developer <william_limaa@hotmail.com>
2025-06-30 11:47:47 -0300

    chore: atualizar titulo e subtitulo do swagger OpenApiConfig

--- src/main/java/br/com/dbc/vemser/imperiodasfichas/config/OpenApiConfig.java
+++ src/main/java/br/com/dbc/vemser/imperiodasfichas/config/OpenApiConfig.java
@@ -17,8 +17,8 @@ public class OpenApiConfig {
     public OpenAPI springShopOpenAPI() {
         String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .info(new Info().title("Pessoa API")
-                        .description("Pessoa API documentação")
+                .info(new Info().title("Império das Fichas API")
+                        .description("Império das fichas documentação")
                         .version("v1.0.0")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
@@ -33,5 +33,4 @@ public class OpenApiConfig {
                                 )
                 );
     }
-
 }

46cfe7356bf67bb43d3a180a714579280314e452
saracristinas <sarasales17062000@gmail.com>
2025-07-12 16:41:53 -0300

    feat(auth): cria AuthController com endpoints para autenticação e criação de usuário

---
+++ src/main/java/br/com/dbc/vemser/imperiodasfichas/controllers/AuthController.java
@@ -0,0 +1,77 @@
+package br.com.dbc.vemser.imperiodasfichas.controllers;
+
+
+
+import br.com.dbc.vemser.imperiodasfichas.dtos.autenticacao.LoginDTO;
+import br.com.dbc.vemser.imperiodasfichas.dtos.autenticacao.RegisterDTO;
+import br.com.dbc.vemser.imperiodasfichas.entities.UsuarioEntity;
+import br.com.dbc.vemser.imperiodasfichas.exceptions.RegraDeNegocioException;
+import br.com.dbc.vemser.imperiodasfichas.security.AuthenticationService;
+import br.com.dbc.vemser.imperiodasfichas.security.TokenService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@Validated
+@RequiredArgsConstructor
+public class AuthController {
+    public final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public String auth(@RequestBody @Valid LoginDTO loginDTO) throws RegraDeNegocioException {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        loginDTO.getLogin(),
+                        loginDTO.getSenha()
+                );
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        usernamePasswordAuthenticationToken);
+
+        UsuarioEntity usuarioValidado = (UsuarioEntity) authentication.getPrincipal();
+
+        return tokenService.generateToken(usuarioValidado);
+    }
+
+    //TODO CRIAR USUARIO
+    @PostMapping("/register")
+    public ResponseEntity<String> createUser(@RequestBody @Valid RegisterDTO registerDTO) throws RegraDeNegocioException {
+        log.info("Registrando usuário: {}", registerDTO.getLogin());
+
+        LoginDTO loginDTO = authenticationService.register(registerDTO);
+        log.info("Usuário registrado. Tentando autenticar...");
+
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            registerDTO.getLogin(),
+                            registerDTO.getSenha()
+                    )
+            );
+            log.info("Autenticação OK. Gerando token...");
+
+            String token = tokenService.generateToken((UsuarioEntity) auth.getPrincipal());
+            log.info("Token gerado com sucesso");
+
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            log.error("Falha na autenticação: {}", e.getMessage(), e);
+            throw new RegraDeNegocioException("Falha ao autenticar usuário");
+        }
+    }
+}
